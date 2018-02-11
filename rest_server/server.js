2ef2e27ddaa422235340728cd7a3e7e6f142be45
Daniel Chong <danchong625@gmail.com>
2018-02-11 15:59:11 -0800

    [feat] adds router for api calls

--- rest_server/server.js
+++ rest_server/server.js
@@ -4,11 +4,15 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import morgan from 'morgan';
 
+import { Router } from './routes';
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('short'));
 app.use(cors());
+
+app.use('/api', Router);
 
 export { app };
