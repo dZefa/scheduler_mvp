23bc1fef021a71468e214cbaf5feda750471ffa1
Daniel Chong <danchong625@gmail.com>
2018-02-12 10:16:13 -0800

    [feat] initializes calendar components

---
+++ client/src/components/calendar/calendar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class CalanderTab extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {  } = this.props;
+    return (
+      <div>Calendar Tab</div>
+    )
+  }
+};
+
+export default CalanderTab;
---
+++ client/src/components/calendar/calendarView.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CalendarView extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+  }
+};
+
+export default CalendarView;
