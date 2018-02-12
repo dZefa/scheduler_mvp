b6854dd0369ccc5b15c69cba3c946face56f27a7
Daniel Chong <danchong625@gmail.com>
2018-02-12 10:16:29 -0800

    [ops] implements redux and getroomdata

--- client/src/components/landing/landing.jsx
+++ client/src/components/landing/landing.jsx
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import { getAllRoomData } from '../../actions/roomActions';
 
 class LandingPage extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+    const { getRoomData, roomData } = this.props;
+
+    if (roomData.length === 0) {
+      getRoomData();
+    }
   }
 
   render() {
+    const { roomData } = this.props;
+
     return (
-      <div>Landing Page!</div>
+      <div>
+        Landing
+      </div>
     )
   }
 };
 
-export default LandingPage;
+const LandingState = (state) => {
+  return {
+    roomData: state.room.roomData,
+  }
+};
+
+const LandingDispatch = (dispatch) => {
+  return {
+    getRoomData: bindActionCreators(getAllRoomData, dispatch),
+  }
+};
+
+export default connect(LandingState, LandingDispatch)(LandingPage);
