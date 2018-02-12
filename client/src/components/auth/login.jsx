0ac079c1456bb06601df716ff2c2814a3b333b95
Daniel Chong <danchong625@gmail.com>
2018-02-12 14:57:33 -0800

    [feat] adds form to login

--- client/src/components/auth/login.jsx
+++ client/src/components/auth/login.jsx
@@ -1,27 +1,63 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { push } from 'react-router-redux';
+import { Redirect } from 'react-router-dom';
 
 import * as authActions from '../../actions/authActions';
 
 class LoginPage extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      username: '',
+      password: ''
+    };
+
+    this.updateUsername = this.updateUsername.bind(this);
+    this.updatePW = this.updatePW.bind(this);
+  }
+
+  updateUsername(text) {
+    this.setState({
+      username: text,
+    });
+  }
+
+  updatePW(text) {
+    this.setState({
+      password: text,
+    });
   }
 
   render() {
     const { actions, authorized } = this.props;
+    const { username, password } = this.state;
 
     if (authorized) {
-      push('/');
       return (
-        <div>Already Logged In</div>
+        <Redirect to="/" />
       );
     }
 
     return (
-      <div>LOGIN PAGE</div>
+      <div>
+        <form>
+          <input type="text" placeholder="Username" onChange={(e) => {this.updateUsername(e.target.value)}} />
+          <input type="password" placeholder="Password" onChange={(e) => {this.updatePW(e.target.value)}} />
+          <button type="submit" 
+            onClick={(e) => {
+              e.preventDefault();
+              const userObj = {
+                login: username,
+                password: password
+              };
+              actions.authLogin(userObj);
+            }}>
+            SUBMIT
+          </button>
+        </form>
+      </div>
     )
   }
 };
