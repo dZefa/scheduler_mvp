9186a6871422820db825c534cf3182425cdcef88
Daniel Chong <danchong625@gmail.com>
2018-02-11 20:59:18 -0800

    [ops] initializes webpack for dev and prod

---
+++ client/webpack.dev.config.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack');
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+const APP_DIR = path.resolve(__dirname, './src/index.jsx');
+const BUILD_DIR = path.resolve(__dirname, './dist');
+const TEMPLATE_DIR = path.resolve(__dirname, './src/template.index.html');
+const ENV_DIR = path.resolve(__dirname, './.env');
+
+const cleanOptions = {
+  verbose: true,
+};
+
+module.exports = {
+  entry: {
+    app: APP_DIR,
+  },
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: TEMPLATE_DIR,
+      inject: 'body',
+    }),
+    new Dotenv({
+      path: ENV_DIR,
+      safe: false,
+    })
+  ]
+}
