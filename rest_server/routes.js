7f46d6338565c8025f4a099076023a13a7f1fa95
Daniel Chong <danchong625@gmail.com>
2018-02-11 17:44:00 -0800

    [ops] adds timeslot controller routes

--- rest_server/routes.js
+++ rest_server/routes.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 import { checkLogin, createLogin, updateLogin } from './controllers/accountController';
 import { createRoom, removeRoom, updateRoom, getAllRoomTimeslots } from './controllers/roomController';
+import { scheduleTimeslot, cancelTimeslot, updateTimeslotInfo } from './controllers/timeslotController';
 
 const Router = express.Router();
 
@@ -19,5 +20,12 @@ Router.route('/room')
 Router.route('/room/:id')
   .delete(removeRoom)
   .put(updateRoom);
+
+Router.route('/timeslot')
+  .post(scheduleTimeslot);
+
+Router.route('/timeslot/:id')
+  .delete(cancelTimeslot)
+  .put(updateTimeslotInfo);
 
 export { Router };
