d9b95d18e7d25b825e44861a409fe373bf83d0ec
Daniel Chong <danchong625@gmail.com>
2018-02-11 17:21:07 -0800

    [ops] adds route to get all timeslots for all rooms

--- rest_server/routes.js
+++ rest_server/routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import { checkLogin, createLogin, updateLogin } from './controllers/accountController';
-import { createRoom, removeRoom, updateRoom } from './controllers/roomController';
+import { createRoom, removeRoom, updateRoom, getAllRoomTimeslots } from './controllers/roomController';
 
 const Router = express.Router();
 
@@ -13,6 +13,7 @@ Router.route('/signup')
   .post(createLogin);
 
 Router.route('/room')
+  .get(getAllRoomTimeslots)
   .post(createRoom);
 
 Router.route('/room/:id')
