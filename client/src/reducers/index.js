055f26b1cdde3db5af4e242d988f5714f8ac5bef
Daniel Chong <danchong625@gmail.com>
2018-02-12 14:33:22 -0800

    [ops] adds auth reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
 
 import { roomReducer as room } from './roomReducer';
+import { authReducer as auth } from './authReducer';
 
 const Reducer = combineReducers({
+  auth,
   room,
   router,
 });
