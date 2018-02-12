09c0ecf7f5a63c23da9a5400d5d53757c62c3498
Daniel Chong <danchong625@gmail.com>
2018-02-12 11:20:01 -0800

    [feat] localizes calendar with moment

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
+import localizer from 'react-big-calendar/lib/localizers/moment'
+import moment from 'moment';
 
 import { configureStore, history } from './store/configureStore';
 
 import './global.css';
 
 import App from './components/app/app.jsx';
+
+localizer(moment);
 
 const store = configureStore();
 
