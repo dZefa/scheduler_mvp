7b552c1546dbf920762cae096dc173c02dcb5325
Daniel Chong <danchong625@gmail.com>
2018-02-11 22:35:21 -0800

    [ops] initializes landing page

---
+++ client/src/components/landing/landing.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class LandingPage extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>Landing Page!</div>
+    )
+  }
+};
+
+export default LandingPage;
