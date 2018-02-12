088c940355be6d4ce0efe460cf0422938528bb16
Daniel Chong <danchong625@gmail.com>
2018-02-12 11:10:28 -0800

    [ops] adds loader and renders conditionally

--- client/src/components/calendar/calendarView.jsx
+++ client/src/components/calendar/calendarView.jsx
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 
+import CalendarTab from './calendar.jsx';
+import Loading from '../loading/loading.jsx';
+
 class CalendarView extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
+    const { roomData } = this.props;
 
+    if ( roomData.length === 0) {
+      return (
+        <Loading />
+      )
+    } 
+
+    return (
+      <div>
+        {
+          roomData.map((room, i) => {
+            return <CalendarTab key={`${room.name + i}`} name={room.name} id={room.id} timeslots={room.timeslots} />;
+          })
+        }
+      </div>
+    )
   }
 };
 
