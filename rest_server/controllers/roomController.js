06a3545a78544085a4eafe2ba2dcf96401ca72a1
Daniel Chong <danchong625@gmail.com>
2018-02-11 17:22:16 -0800

    [ops] adds method to grab all timeslots for all rooms

--- rest_server/controllers/roomController.js
+++ rest_server/controllers/roomController.js
@@ -1,4 +1,5 @@
-import { addRoom, deleteRoom, updateRoom } from '../database/controllers/roomController';
+import { addRoom, deleteRoom, updateRoom, getRooms } from '../database/controllers/roomController';
+import { getTimeslots } from '../database/controllers/timeslotController';
 
 const createRoom = (req, res) => {
   addRoom(req.body.name)
@@ -30,4 +31,28 @@ const updateRoomName = (req, res) => {
     });
 };
 
-export { createRoom, removeRoom, updateRoom };
+const getAllRoomTimeslots = (req, res) => {
+  const roomInfo = [];
+
+  getRooms()
+    .then(rooms => {
+      for (let i = 0; i < rooms.length; i++) {
+        roomInfo[i] = {
+          name: rooms[i].name,
+          timeslots: []
+        };
+        getTimeslots(rooms[i].id)
+          .then(timeslots => {
+            for(let j = 0; j < timeslots.length; j++) {
+              roomInfo[i].timeslots.push(timeslots[j]);
+            }
+          });
+      }
+      res.status(200).send({ result: roomInfo });
+    })
+    .catch(err => {
+      res.sendStatus(500);
+    })
+}
+
+export { createRoom, removeRoom, updateRoom, getAllRoomTimeslots };
