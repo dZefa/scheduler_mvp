cce82dba69ec2358b57cbbb5203e97ef7133e308
Daniel Chong <danchong625@gmail.com>
2018-02-13 09:37:42 -0800

    [ops] adds groupcontacts component to routes

--- client/src/components/app/routes.jsx
+++ client/src/components/app/routes.jsx
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import LandingPage from '../landing/landing.jsx';
 import LoginPage from '../auth/login.jsx';
 import ManagePage from '../manage/manageView.jsx';
+import GroupContactsPage from '../groupContacts/groupContactsView.jsx';
 
 class Routes extends Component {
   constructor() {
@@ -16,6 +17,7 @@ class Routes extends Component {
         <Route exact path="/" component={LandingPage} />
         <Route path="/login" component={LoginPage} />
         <Route path="/manage" component={ManagePage} />
+        <Route path="/contacts" component={GroupContactsPage} />
       </Switch>
     )
   }
