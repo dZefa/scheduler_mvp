883764f53e75796de17d50c8a22adda4f10d1296
Daniel Chong <danchong625@gmail.com>
2018-02-12 16:25:30 -0800

    [feat] implements admin/group nav && logout

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { Link } from 'react-router-dom';
+
+import { authLogout } from '../../actions/authActions';
+
+import AdminNav from './adminNav.jsx';
+import GroupNav from './groupNav.jsx';
 
 class Navbar extends Component {
   constructor(props) {
@@ -8,13 +14,36 @@ class Navbar extends Component {
   }
 
   render() {
+    const { authorized, logout, type } = this.props;
+
     return (
       <div>
         <Link to="/">HOME</Link>
         <Link to="/login" >LOGIN</Link>
+        { authorized && type === 'admin' && <AdminNav />}
+        { authorized && type === 'group' && <GroupNav />}
+        { authorized && 
+          <div onClick={(e) => {
+            e.preventDefault();
+            logout();
+          }}>LOGOUT</div>
+        }
       </div>
     )
   }
 };
 
-export default connect()(Navbar);
+const NavState = (state) => {
+  return {
+    authorized: state.auth.authorized,
+    type: state.auth.user.type,
+  }
+};
+
+const NavDispatch = (dispatch) => {
+  return {
+    logout: bindActionCreators(authLogout, dispatch),
+  }
+};
+
+export default connect(NavState, NavDispatch)(Navbar);
