944fffdd2021e44f48c4945af0b2a2bbb891b137
Daniel Chong <danchong625@gmail.com>
2018-02-15 14:55:32 -0800

    [feat] implements call to server to delete timeslot

--- client/src/components/timeslot/removeTimeslot.jsx
+++ client/src/components/timeslot/removeTimeslot.jsx
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class RemoveTimeslotsView extends Component {
   constructor(props) {
     super(props);
   }
 
+  deleteTimeslot(id) {
+    const { refreshPage } = this.props;
+
+    axios.delete(`${process.env.REST_SERVER_URL}/api/timeslot/${id}`)
+      .then(success => {
+        refreshPage();
+      })
+      .catch(err => {
+        console.log(`Error inside deleteTimeslot. Error: ${err}`);
+      });
+  }
+
   render() {
+    const { userTimeslot } = this.props;
+
     return (
-      <div>hi</div>
+      <div>
+        <input value={`Timeslot: ${userTimeslot.title} ${userTimeslot.start} - ${userTimeslot.end}`} disabled={true}/>
+        <button type="button" onClick={(e) => {
+          e.preventDefault();
+          this.deleteTimeslot(userTimeslot.id);
+        }} >Cancel Reservation</button>
+      </div>
     )
   }
 };
